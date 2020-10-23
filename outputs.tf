7c38fb68abd5220c2d28af6357a17ced1f6c387a
beermann <beermann@b1-systems.de>
2020-10-23 12:49:13 +0200

    feature with output added

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "debian_fips" {
+  value = module.vms_debian.fip
+}
+
+output "centos_fips" {
+  value = module.vms_centos.fip
+}
+
+output "opensuse_fips" {
+  value = module.vms_opensuse.fip
+}
+
--- variables.tf
+++
@@ -1,3 +0,0 @@
-variable "counter" {
-  default = 1
-}
